ad4831170992233b32d524c814ec3a88febef0a1
Anthony Maddatu <amaddatu@gmail.com>
2019-04-04 18:57:25 -0400

    Add bootstrap to demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HelloBootstrap from "./components/HelloBootstrap";
+import BootstrapCSS from "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return <HelloBootstrap />;
