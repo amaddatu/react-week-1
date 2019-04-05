f80d074a95a6b974350046c6c2df3ac85fe6f9bb
Anthony Maddatu <amaddatu@gmail.com>
2019-04-04 21:24:58 -0400

    Add activity 20

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function JSXVariables() {
+  const name="Huh What Example What What What";
+  //const lengthOfName = name.length;
+  const thoughts="its great";
   return (
+    
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
-          <h1>Hi! My name is (insert name here)</h1>
-          <h2>My name has (insert number of letters in name here) letters</h2>
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h1>Hi! My name is {name}</h1>
+          <h2>My name has {name.length} letters</h2>
+          <h2>I think React {thoughts}</h2>
+          {name.split('').map(letter=>{return (<p>{letter}</p>);})}
         </div>
       </div>
     </div>
